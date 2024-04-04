bf2481c26318c8ab99bba4b5541b4aa9bac45ce8
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-04-03 21:02:30 -0500

    Build: Recieve CLI arguments with Clap
    Start the get_args() function to recieve the desired arguments from the
    user

--- rusty_stocks/src/lib.rs
+++ rusty_stocks/src/lib.rs
@@ -8,3 +8,28 @@ pub struct Config {
     tickers: Vec<String>,
     days: Option<usize>,
 }
+
+pub fn get_args() -> CustomResult<Config> {
+    let mut matches = Command::new("rusty_stocks")
+        .version("0.1.0")
+        .author("Derek Warner <derekw3@illinois.edu>, Chengxun Ren <cren8@illinois.edu>, Haozhe Chen <haozhe6@illinois.edu>, Aaryan Singh Gusain <agusain2@illinois.edu>")
+        .about("A CLI stock prediction application")
+        .arg(
+            Arg::new("tickers")
+                .help("Stock Ticker(s)")
+                .default_value("-")
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("number_days")
+                .short('n')
+                .long("days")
+                .value_name("DAYS")
+                .help("Number of days to predict")
+                .num_args(1)
+                .default_value("10"),   
+        )
+        .get_matches();
+
+    Ok(())
+}

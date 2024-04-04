56ebacdf4eba0fe01a4b60854549921ef4a0a3e8
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-04-03 20:47:44 -0500

    Build: Initial Build
    Run cargo new to create project and include clap for CLI interface

---
+++ rusty_stocks/target/debug/deps/clap-a9732e418d9f2abb.d
@@ -0,0 +1,7 @@
+/Users/derekwarner/repos/cs128H/CS128_project/rusty_stocks/target/debug/deps/libclap-a9732e418d9f2abb.rmeta: /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.4/src/lib.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.4/src/../examples/demo.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.4/src/../examples/demo.md
+
+/Users/derekwarner/repos/cs128H/CS128_project/rusty_stocks/target/debug/deps/clap-a9732e418d9f2abb.d: /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.4/src/lib.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.4/src/../examples/demo.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.4/src/../examples/demo.md
+
+/Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.4/src/lib.rs:
+/Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.4/src/../examples/demo.rs:
+/Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.4/src/../examples/demo.md:
---
+++ rusty_stocks/target/debug/deps/rusty_stocks-d36ff0a50563d73d.d
@@ -0,0 +1,9 @@
+/Users/derekwarner/repos/cs128H/CS128_project/rusty_stocks/target/debug/deps/librusty_stocks-d36ff0a50563d73d.rmeta: src/main.rs Cargo.toml
+
+/Users/derekwarner/repos/cs128H/CS128_project/rusty_stocks/target/debug/deps/rusty_stocks-d36ff0a50563d73d.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR

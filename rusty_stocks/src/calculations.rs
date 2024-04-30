1a64f4bae4cbcfcc838d83507e63b15535c6dc0e
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-04-29 19:34:06 -0500

    Build: Implement split_data calculation function

--- rusty_stocks/src/calculations.rs
+++ rusty_stocks/src/calculations.rs
@@ -1,8 +1,11 @@
+use std::intrinsics::floorf32;
+
 use crate::stock::{self, Stock};
 
 use clap::Error;
+use rand::seq::SliceRandom;
 use randomforest::criterion::Mse;
-use randomforest::table::{self, Table, TableBuilder, TableError};
+use randomforest::table::{Table, TableBuilder, TableError};
 use randomforest::RandomForestRegressorOptions;
 
 pub fn construct_table(stocks: &Vec<Stock>) -> TableBuilder {
@@ -15,6 +18,19 @@ pub fn construct_table(stocks: &Vec<Stock>) -> TableBuilder {
     table_builder
 }
 
-fn split_data(stocks: &Vec<Stock>) -> (Vec<Stock>, Vec<Stock>) {
-    
+fn split_data(stocks: &Vec<Stock>, training: f32) -> (Vec<Stock>, Vec<Stock>) {
+    let mut indices: Vec<usize> = (0..stocks.len()).collect();
+    indices.shuffle(&mut rand::thread_rng());
+    let training_index: usize = (training * (stocks.len() as f32)) as usize;
+    let mut training_set: Vec<Stock> = Vec::new();
+    for idx in indices[0..training_index].iter() {
+        training_set.push(stocks[*idx]);
+    }
+
+    let mut test_set: Vec<Stock> = Vec::new();
+    for idx in indices[training_index..].iter() {
+        test_set.push(stocks[*idx]);
+    }
+
+    (training_set, test_set)
 }

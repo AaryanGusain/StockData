97e3cf0c0cd2e41342ecaed532e1cd00cc68fed6
HZCh03 <haozhechen0@gmail.com>
2024-05-02 23:18:11 -0500

    implemented plotting

--- rusty_stocks/src/main.rs
+++ rusty_stocks/src/main.rs
@@ -3,11 +3,9 @@ mod plot;
 use std::env;
 
 fn main() {
-
-    
-    // Plot the <data.csv> if the command input is "cargo run plot <path to data.csv>"
     let args: Vec<String> = env::args().collect();
     if args.len() == 3 && args[1] == "plot" {
+        // Plot the <data.csv> if the command input is "cargo run plot <path to data.csv>"
         let file_path = &args[2];
         if let Err(err) = plot::plot_csv(file_path) {
             eprintln!("Error: {}", err);
